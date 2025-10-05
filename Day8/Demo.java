3986366405423ee2c5bf8e910d65c2d17f0ce45e
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-10-05 22:46:28 +0530

    Da8

--- Day8/Demo.java
+++ Day8/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("day8");
         System.out.println("hello");
+        System.out.println("ankit");
     }
 }
