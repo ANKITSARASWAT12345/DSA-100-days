f5e34531d5f541ad37693dd4ecf52ce6220fe94b
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-10-06 23:49:06 +0530

    Day8

--- Day 9/LongestSubarray.java
+++ Day 9/LongestSubarray.java
@@ -82,7 +82,7 @@ Given the integer array cardPoints and the integer k, return the maximum score y
 
 
 
-class LongestSubarray {
+/*class LongestSubarray {
 
     public static void main(String[] args) {
         int cardPoints[] = {1,2,3,4,5,6,1}; 
@@ -111,3 +111,4 @@ class LongestSubarray {
         return maxSum;
     }
 }
+*/
