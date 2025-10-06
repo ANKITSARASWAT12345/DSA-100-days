72c2df3839f4aeb20405d7e14ed6203a6d1acd45
Ankit Saraswat <ankit1256saraswat@gmail.com>
2025-10-06 23:43:35 +0530

    Da8

--- Day 9/LongestSubarray.java
+++ Day 9/LongestSubarray.java
@@ -39,7 +39,7 @@
 //Optimal Solution
 //Time complexity  O(n)
 
-public class LongestSubarray {
+/*public class LongestSubarray {
     public static void main(String[] args) {
         int arr[]={2,5,1,10,10};
         int k=14;
@@ -67,4 +67,47 @@ public class LongestSubarray {
         return maxLen;
     }
 }
+*/
 
+
+//Maximum Points You Can Obtain from Cards
+/*There are several cards arranged in a row, and each card has an associated number of points. The points are given in the integer array cardPoints.
+
+In one step, you can take one card from the beginning or from the end of the row. You have to take exactly k cards.
+
+Your score is the sum of the points of the cards you have taken.
+
+Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
+*/
+
+
+
+class LongestSubarray {
+
+    public static void main(String[] args) {
+        int cardPoints[] = {1,2,3,4,5,6,1}; 
+        int k = 3;
+        System.out.println(maxScore(cardPoints, k));
+
+    }
+    public static int maxScore(int[] cardPoints, int k) {
+        int leftSum=0;
+        int rightSum=0;
+        int maxSum=0;
+
+        for(int i=0;i<k;i++){
+            leftSum+=cardPoints[i];
+        }
+        maxSum=leftSum;
+        
+        int endIndex=cardPoints.length-1;
+
+        for(int i= k-1;i>=0;i--){
+            leftSum=leftSum-cardPoints[i];
+             rightSum+=cardPoints[endIndex];
+             endIndex--;
+             maxSum=Math.max(maxSum,leftSum+rightSum);
+        }
+        return maxSum;
+    }
+}
